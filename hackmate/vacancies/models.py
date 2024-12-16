98ed941b7c2db5452d162657f4a847f5e9d5936e
Левон Харазян <l.kharazyan@yandex.ru>
2024-12-16 16:22:59 +0000

    теперь точно

--- hackmate/vacancies/models.py
+++ hackmate/vacancies/models.py
@@ -1,166 +1,175 @@
-__all__ = []
-
-import pathlib
-import re
-import sys
+__all__ = ()
 
 import django.conf
-import django.contrib.auth.models
+import django.core.validators
 import django.db.models
 import django.utils.translation
 
-import users.validators
 
+class Ip(django.db.models.Model):
+    ip = django.db.models.CharField(max_length=100)
 
-if "makemigrations" not in sys.argv and "migrate" not in sys.argv:
-    email_field = django.contrib.auth.models.User._meta.get_field("email")
-    email_field._unique = True
-
-    username_field = django.contrib.auth.models.User._meta.get_field(
-        "username",
-    )
-    username_field._unique = True
-
-    username_field = django.contrib.auth.models.User._meta.get_field(
-        "password",
-    )
-    username_field._max_length = 128
-
-
-def user_directory_path(instance, filename):
-    username = instance.user.username
-    return pathlib.Path("uploads") / "users_logos" / username / filename
-
-
-class UserManager(django.contrib.auth.models.UserManager):
-    def normalize_email(self, email):
-        email = super().normalize_email(email)
-        email = email.lower()
-        local_part, domain = email.split("@")
-
-        local_part = local_part.split("+")[0]
-
-        if domain in ["yandex.ru", "ya.ru"]:
-            domain = "yandex.ru"
-            local_part = local_part.replace(".", "-")
-        elif domain == "gmail.com":
-            local_part = local_part.replace(".", "")
-
-        local_part = re.sub(r"\+.*", "", local_part)
-        return f"{local_part}@{domain}"
-
-    def active(self):
-        return (
-            self.get_queryset()
-            .filter(is_active=True)
-            .select_related("profile")
-        )
-
-    def inactive(self):
-        return (
-            self.get_queryset()
-            .filter(is_active=False)
-            .select_related("profile")
-        )
-
-    def by_mail(self, identifier):
-        normalized_email = self.normalize_email(identifier)
-        return self.active().filter(email=normalized_email).first() or None
-
-
-class User(django.contrib.auth.models.User):
-    objects = UserManager()
+    def __str__(self):
+        return self.ip
 
     class Meta:
-        proxy = True
+        verbose_name = django.utils.translation.gettext_lazy("IP адрес")
+        verbose_name_plural = django.utils.translation.gettext_lazy(
+            "IP адреса",
+        )
 
 
-class Profile(django.db.models.Model):
-    user = django.db.models.OneToOneField(
+class Vacancy(django.db.models.Model):
+    class VacancyStatuses(django.db.models.TextChoices):
+        ACTIVE = "active", "active"
+        INACTIVE = "inactive", "inactive"
+
+    creater = django.db.models.ForeignKey(
         django.conf.settings.AUTH_USER_MODEL,
         on_delete=django.db.models.CASCADE,
-        related_name="profile",
-    )
-
-    birthday = django.db.models.DateField(
-        null=True,
-        blank=True,
-        verbose_name=django.utils.translation.gettext_lazy("день рождения"),
-        help_text=django.utils.translation.gettext_lazy(
-            "Введите дату рождения",
+        verbose_name=django.utils.translation.gettext_lazy(
+            "создатель вакансии",
         ),
-        validators=[users.validators.validate_birthday],
     )
 
-    image = django.db.models.ImageField(
-        upload_to=user_directory_path,
-        null=True,
-        blank=True,
-    )
-
-    attempts_count = django.db.models.PositiveIntegerField(
-        default=0,
+    title = django.db.models.CharField(
+        max_length=255,
+        verbose_name=django.utils.translation.gettext_lazy(
+            "название вакансии",
+        ),
     )
 
     description = django.db.models.TextField(
+        verbose_name=django.utils.translation.gettext_lazy(
+            "описание вакансии",
+        ),
+    )
+
+    created_at = django.db.models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=django.utils.translation.gettext_lazy("создано"),
+    )
+
+    updated_at = django.db.models.DateTimeField(
+        auto_now=True,
+        verbose_name=django.utils.translation.gettext_lazy("обновлено"),
+    )
+
+    status = django.db.models.CharField(
+        max_length=255,
+        choices=VacancyStatuses.choices,
+        verbose_name=django.utils.translation.gettext_lazy("cтатус"),
+    )
+
+    views = django.db.models.ManyToManyField(
+        Ip,
+        related_name="post_views",
+        blank=True,
+    )
+
+    hackaton_title = django.db.models.CharField(
+        max_length=255,
+        verbose_name=django.utils.translation.gettext_lazy(
+            "название хакатона",
+        ),
+        blank=True,
+        null=True,
+        help_text=django.utils.translation.gettext_lazy(
+            "Название хакатона, к которому относится вакансия",
+        ),
+    )
+
+    deadline = django.db.models.DateField(
+        verbose_name=django.utils.translation.gettext_lazy("дедлайн"),
         null=True,
         blank=True,
-        verbose_name=django.utils.translation.gettext_lazy("о себе"),
-        help_text=django.utils.translation.gettext_lazy("Расскажите о себе"),
-    )
-
-    date_last_active = django.db.models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        verbose_name = django.utils.translation.gettext_lazy(
-            "дополнительная информация",
-        )
-        verbose_name_plural = django.utils.translation.gettext_lazy(
-            "дополнительные данные",
-        )
-
-
-class ProfileLink(django.db.models.Model):
-    SOCIAL_NETWORKS = [
-        ("facebook", "Facebook"),
-        ("twitter", "Twitter"),
-        ("instagram", "Instagram"),
-        ("vk", "VK"),
-        ("gitlub", "GitLub"),
-        ("github", "GitHub"),
-    ]
-
-    profile = django.db.models.ForeignKey(
-        Profile,
-        on_delete=django.db.models.CASCADE,
-        related_name="links",
-        verbose_name=django.utils.translation.gettext_lazy("профиль"),
-    )
-    site_type = django.db.models.CharField(
-        max_length=20,
-        choices=SOCIAL_NETWORKS,
-        verbose_name=django.utils.translation.gettext_lazy("тип сайта"),
-    )
-    url = django.db.models.URLField(
-        verbose_name=django.utils.translation.gettext_lazy("URL"),
         help_text=django.utils.translation.gettext_lazy(
-            "Полная ссылка, например, https://example.com",
+            "Крайний срок подачи заявок",
+        ),
+    )
+
+    required_experience = django.db.models.IntegerField(
+        verbose_name=django.utils.translation.gettext_lazy(
+            "требуемый опыт (в годах)",
         ),
         null=True,
         blank=True,
+        help_text=django.utils.translation.gettext_lazy(
+            "Укажите количество лет опыта, необходимого для кандидата",
+        ),
+        validators=[django.core.validators.MinValueValidator(0)],
     )
 
-    def get_fa_icon_class(self):
-        fa_icons = {
-            "facebook": "fa-facebook",
-            "twitter": "fa-twitter",
-            "instagram": "fa-instagram",
-            "vk": "fa-vk",
-            "gitlub": "fa-gitlab",
-            "github": "fa-github",
-        }
-        return fa_icons.get(self.site_type, "fa-link")
+    class Meta:
+        verbose_name = django.utils.translation.gettext_lazy("вакансия")
+        verbose_name_plural = django.utils.translation.gettext_lazy("вакансии")
+        ordering = ["-created_at"]
+
+    def total_views(self):
+        return self.views.count()
+
+    def __str__(self):
+        return self.title
+
+
+class CommentVacancy(django.db.models.Model):
+    vacancy = django.db.models.ForeignKey(
+        "Vacancy",
+        on_delete=django.db.models.CASCADE,
+        verbose_name=django.utils.translation.gettext_lazy("вакансия"),
+        related_name="comments",
+    )
+
+    comment = django.db.models.TextField(
+        verbose_name=django.utils.translation.gettext_lazy("комментарий"),
+    )
+
+    user = django.db.models.ForeignKey(
+        django.conf.settings.AUTH_USER_MODEL,
+        on_delete=django.db.models.CASCADE,
+        verbose_name=django.utils.translation.gettext_lazy("пользователь"),
+    )
+
+    created_at = django.db.models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=django.utils.translation.gettext_lazy("cоздано"),
+    )
 
     class Meta:
-        verbose_name = django.utils.translation.gettext_lazy("ссылка")
-        verbose_name_plural = django.utils.translation.gettext_lazy("ссылки")
+        verbose_name = django.utils.translation.gettext_lazy(
+            "комментарий к вакансии",
+        )
+        verbose_name_plural = django.utils.translation.gettext_lazy(
+            "комментарии к вакансиям",
+        )
+        ordering = ["-created_at"]
+
+
+class Response(django.db.models.Model):
+    user = django.db.models.ForeignKey(
+        django.conf.settings.AUTH_USER_MODEL,
+        on_delete=django.db.models.CASCADE,
+        related_name="responses",
+        verbose_name=django.utils.translation.gettext_lazy("пользователь"),
+    )
+    vacancy = django.db.models.ForeignKey(
+        "Vacancy",
+        on_delete=django.db.models.CASCADE,
+        related_name="responses",
+        verbose_name=django.utils.translation.gettext_lazy("вакансия"),
+    )
+    created_at = django.db.models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=django.utils.translation.gettext_lazy("дата отклика"),
+    )
+
+    class Meta:
+        verbose_name = django.utils.translation.gettext_lazy("отклик")
+        verbose_name_plural = django.utils.translation.gettext_lazy("отклики")
+        unique_together = (
+            "user",
+            "vacancy",
+        )
+
+    def __str__(self):
+        return f"{self.user} -> {self.vacancy}"
