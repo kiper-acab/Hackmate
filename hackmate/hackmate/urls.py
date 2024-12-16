2125ff6602c44232404a7db62785e9fa9c8eedaf
Левон Харазян <l.kharazyan@yandex.ru>
2024-12-16 16:47:59 +0000

    вернул ссылки

--- hackmate/hackmate/urls.py
+++ hackmate/hackmate/urls.py
@@ -2,6 +2,7 @@ import django.conf
 import django.conf.urls.static
 import django.contrib.admin
 import django.urls
+import notifications.urls
 
 
 if django.conf.settings.DEBUG:
@@ -21,8 +22,18 @@ urlpatterns += [
     django.urls.path("", django.urls.include("homepage.urls")),
     django.urls.path("auth/", django.urls.include("users.urls")),
     django.urls.path("vacancy/", django.urls.include("vacancies.urls")),
+    django.urls.path("notification/", django.urls.include("notify.urls")),
+    django.urls.path("api/", django.urls.include("api.urls")),
     django.urls.path("about/", django.urls.include("about.urls")),
     django.urls.path("admin/", django.contrib.admin.site.urls),
+    django.urls.re_path(
+        "^inbox/notifications/",
+        django.urls.include(notifications.urls, namespace="notifications"),
+    ),
+    django.urls.path(
+        "ratings/",
+        django.urls.include("star_ratings.urls", namespace="ratings"),
+    ),
 ]
 
 urlpatterns += django.conf.urls.static.static(
