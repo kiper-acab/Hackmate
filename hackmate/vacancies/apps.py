abb6f6125e5b54b040a529908e7c93ae03448080
Левон Харазян <l.kharazyan@yandex.ru>
2024-12-16 16:17:43 +0000

    случайно перепутал

--- hackmate/vacancies/apps.py
+++ hackmate/vacancies/apps.py
@@ -1,13 +1,10 @@
-__all__ = []
+__all__ = ()
 
 import django.apps
 import django.utils.translation
 
 
-class UsersConfig(django.apps.AppConfig):
+class VacanciesConfig(django.apps.AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "users"
-    verbose_name = django.utils.translation.gettext_lazy("Пользователи")
-
-    def ready(self):
-        __import__("users.signals")
+    name = "vacancies"
+    verbose_name = django.utils.translation.gettext_lazy("Вакансии")
